c536dcc1a59c9981283942f50c28f420f4d75a5b
moonclash <emladenoff@gmail.com>
2022-12-07 23:00:47 +0200

    grr

--- src/services/helper_services.py
+++ src/services/helper_services.py
@@ -29,16 +29,19 @@ class DateHelper(object):
 
 
 class GoodMorningImage(object):
-  
+
+
   @staticmethod
   def good_morningfy(image, file_name):
+    static_dir = os.path.dirname(__file__)
+    fonts_dir = os.path.join(static_dir, 'fonts/')
     today_in_bulgarian = DateHelper.get_day_in_bulgarian()
     ext_regex = re.compile('\.[a-z]*', re.IGNORECASE)
     image_ext, *_ = re.findall(ext_regex, file_name)
     new_image_name = f'{str(uuid.uuid4())}{image_ext}'
     string_builder = f'Добро утро в {today_in_bulgarian}! \n Поднасям ви топло кафе и усмивки!'
     font_size = round(len(string_builder) * 1.5)
-    font = ImageFont.truetype(os.path.join('fonts', 'adantino.ttf'), font_size)
+    font = ImageFont.truetype(os.path.join(fonts_dir, 'adantino.ttf'), font_size)
     font_options = {
         'font': font,
         'align': 'center',
